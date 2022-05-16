3102f33d181b5510ae03276dd44e5554b3bd8b16
Victor Owiti <viowiti12@gmail.com>
2022-05-16 12:57:18 +0300

    initial routes setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const PORT = process.env.PORT;
+const goalRoutes = require("./routes/goalRoutes");
+
+const app = express();
+
+app.use("/api/goals", goalRoutes);
+
+//test endpoint
+app.get("/", (req, res) => {
+  res.status(200).json("Up and running");
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port: ${PORT}`.bgMagenta);
+});
