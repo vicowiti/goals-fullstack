6af492337f75d114357e74b9f653c37bb1f6ff99
Victor Owiti <viowiti12@gmail.com>
2022-05-16 18:56:58 +0300

    implemented the REST API

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const connect = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB running at ${connect.connection.host}`.bgBlue);
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB running at ${connect.connection.host}`.bgBlue);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
--- backend/src/controllers/goalControllers.js
+++ backend/src/controllers/goalControllers.js
@@ -1,33 +1,63 @@
 const asyncHandler = require("express-async-handler");
 
+const Goal = require("../models/goalModel");
+
 //@Desc:  Get goals
 //@route: GET /api/goals
 //@access: Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get goal` });
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
 });
 
 //@Desc:  Set goal
 //@route: POST /api/goals
 //@access: Private
 const setGoal = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
+  const { text } = req.body;
+  if (!text) {
+    res.status(400);
     throw new Error("Kindly add text in the goals field");
   }
+  console.log("we here");
+  const goal = await Goal.create({ text });
+
+  res.status(200).json(goal);
 });
 
 //@Desc:  Update goal
 //@route: PUT /api/goals/:id
 //@access: Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
+  const id = req.params.id;
+  const goal = await Goal.findById(id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("The resource does not exist.");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedGoal);
 });
 
 //@Desc:  Delete goal
 //@route: DELETE /api/goals/:id
 //@access: Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  const id = req.params.id;
+  const goal = await Goal.findById(id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("The resource does not exist.");
+  }
+
+  const deletedGoal = await Goal.findByIdAndDelete(id);
+  res.status(200).json(deleteGoal);
 });
 
 module.exports = {
