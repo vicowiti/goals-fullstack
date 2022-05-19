7ee719224dc87ec42576265e23258cd32960b40a
Victor Owiti <viowiti12@gmail.com>
2022-05-19 11:21:53 +0300

    added user model

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Please add first name"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please add last name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
